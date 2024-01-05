c432ffc082c1c526afe348bafc0c07470bfd4135
happyrina <riniversal@gmail.com>
2024-01-05 23:18:15 +0900

    TypeConversion

---
+++ TypeConversionTest.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TypeConversionTest {
+    public static void main(String[] args) {
+        String str = "ABC"; //String
+        Integer i = 123; //Integer
+        ArrayList<String> list = new ArrayList<String>(); //ArrayList
+
+        System.out.println(str.getClass().getName());
+        System.out.println(i.getClass().getName());
+        System.out.println(list.getClass().getName()); //Type 확인
+
+    }
+}
