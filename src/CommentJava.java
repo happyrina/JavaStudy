a72709d0ac3a57f755c68f1b6a715093feb7608a
happyrina <riniversal@gmail.com>
2024-01-09 22:49:16 +0900

    JavaStudy

---
+++ src/CommentJava.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java1을 출력한다.
+        //System.out.println("hello java2"); 한 줄 주석 single line comment
+
+        /*
+        System.out.println("hello java3"); 여러 줄 주석 multi line comment
+        System.out.println("hello java");
+         */
+    }
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,12 @@
+public class HelloJava {
+//파일명과 클래스 이름은 같아야함! 클래스는 이후에 딥하게 공부 => 지금은 HelloJava.java 파일 만들었다고 생각하면 된다.
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
+
+//자바는 대소문자 구별! 대소문자 다르면 오류 날 수 있음
+//{} 블록으로 시작과 끝 구분
+//main은 메서드 개념 => 현재는 main이 프로그래밍의 시작점임!
+//System.out.println() 값을 콘솔에 출력하는 기능!
+//자바는 ; 세미콜론으로 끝남! 꼭 붙여주기! 한 문장이 끝나면 붙여주기
